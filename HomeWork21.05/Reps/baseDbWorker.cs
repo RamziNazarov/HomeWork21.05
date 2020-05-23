3fae7b17bf164c600bf51c317b476902c3eb10f1
RamziNazarov <Ramzi.13.09.18@gmail.com>
2020-05-23 07:37:05 +0300

    create and end homework

---
+++ HomeWork21.05/Reps/baseDbWorker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace HomeWork21._05.Reps
+{
+    public abstract class DbWorker<TModel> : IReadAllFromDB<TModel>
+    {
+        public List<TModel> ReadAll()
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(ConString.constring))
+                {
+                    return db.Query<TModel>($"select * from {typeof(TModel).Name}").ToList();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
